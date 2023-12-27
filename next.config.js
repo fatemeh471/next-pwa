f1a2adb99cf6c7c2f77cb1e57a9174ab48c34789
eisavand_fa <esavand.fa@gmail.com>
2023-12-27 19:19:03 +0330

    add config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require("@ducanh2912/next-pwa").default({
     dest: "public",
-
+    cacheOnFrontEndNav: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    disable: false,
+    workboxOptions:{
+        disableDevLogs: true
+    }
   });
 const nextConfig = {}
 
